d0698d5dae4ad768686624377cddfb872affcb83
regmibijan <104678547+regmibijan@users.noreply.github.com>
2023-07-05 22:54:28 +0545

    get product

---
+++ routes/product/get.ts
@@ -0,0 +1,33 @@
+import { z, TypeOf } from 'zod'
+import { Request, Response } from 'express'
+import db from '@/config/prisma'
+import { getPayload } from '@/utils/jwt'
+import { StatusCodes } from 'http-status-codes'
+import { withValidation } from '@/middlewares/validate'
+
+const schema = z.object({
+    query: z.object({
+        pid: z.string(),
+    }),
+})
+
+const proc = async (
+    req: Request,
+    res: Response,
+    input: TypeOf<typeof schema>
+) => {
+    const product = await db.product.findUnique({
+        where: { pid: input.query.pid },
+        select: {
+            pid: true,
+            name: true,
+            description: true,
+            manufacturer: true,
+            unitPrice: true,
+            stock: true,
+        },
+    })
+    return res.status(StatusCodes.OK).json(product)
+}
+
+export const getProduct = withValidation(schema, proc)
